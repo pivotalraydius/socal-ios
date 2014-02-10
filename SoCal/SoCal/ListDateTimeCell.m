462159c2cb06cf8f87b01c3bc68bf9c81745511b
Kale <kale@rs-v.com>
2014-02-10 18:31:12 +0800

    Render Calendar List mode cell

--- SoCal/SoCal/ListDateTimeCell.m
+++ SoCal/SoCal/ListDateTimeCell.m
@@ -8,6 +8,10 @@
 
 #import "ListDateTimeCell.h"
 
+#define VOTE_MAYBE 0
+#define VOTE_YES   1
+#define VOTE_NO    2
+
 @implementation ListDateTimeCell
 
 +(ListDateTimeCell *)newCell {
@@ -39,10 +43,55 @@
 
 -(void)setupUI {
     
+    [Helpers setBorderToView:self.dateLabel borderColor:[UIColor clearColor] borderThickness:0.0 borderRadius:self.dateLabel.frame.size.width/2];
 }
 
 -(void)renderWithDate:(NSDate *)date andVote:(int)vote {
     
+    if (vote == VOTE_YES) {
+        [self.voteColorBar setBackgroundColor:[Helpers softGreenColorWithAlpha:1.0]];
+    }
+    else if (vote == VOTE_NO) {
+        [self.voteColorBar setBackgroundColor:[Helpers softRedColorWithAlpha:1.0]];
+    }
+    else if (vote == VOTE_MAYBE) {
+        [self.voteColorBar setBackgroundColor:[Helpers suriaOrangeColorWithAlpha:1.0]];
+    }
+    
+    NSString *dateStr = @"";
+    NSString *monthStr = @"";
+    NSString *dayTimeStr = @"";
+    
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    
+    [df setAMSymbol:@"am"];
+    [df setPMSymbol:@"pm"];
+    
+    [df setDateFormat:@"dd"];
+    
+    dateStr = [NSString stringWithFormat:@"%@",
+                   [df stringFromDate:date]];
+    
+    [df setDateFormat:@"MMMM"];
+    
+    monthStr = [NSString stringWithFormat:@"%@",
+                     [df stringFromDate:date]];
+    
+    [df setDateFormat:@"eeee, hh:mm a"];
+    
+    dayTimeStr = [NSString stringWithFormat:@"%@",
+                    [df stringFromDate:date]];
+    
+    [self.dateLabel setText:dateStr];
+    [self.monthLabel setText:monthStr];
+    [self.dayTimeLabel setText:dayTimeStr];
+    
+    if ([dayTimeStr hasSuffix:@"am"]) {
+        [self.mainBGView setBackgroundColor:[Helpers suriaOrangeColorWithAlpha:1.0]];
+    }
+    else {
+        [self.mainBGView setBackgroundColor:[Helpers pmBlueColorWithAlpha:1.0]];
+    }
 }
 
 @end

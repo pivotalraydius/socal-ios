c44fa3570e29d2148467bab23978938039559a07
Hnin Htet <hninhtet@rs-v.com>
2014-02-11 16:42:31 +0800

    draw partitions

--- SoCal/SoCal/RDPieView.m
+++ SoCal/SoCal/RDPieView.m
@@ -42,6 +42,7 @@ static inline float radians(double degrees) { return degrees * PI / 180; }
 	
 	startDeg=0;
 	endDeg=(self->m_pmRatio * mult);
+
 	if(startDeg != endDeg)
 	{
         UIColor *color;
@@ -76,11 +77,64 @@ static inline float radians(double degrees) { return degrees * PI / 180; }
 		CGContextAddArc(ctx, x, y, r, (startDeg)*M_PI/180.0, (endDeg)*M_PI/180.0, 0);
 		CGContextClosePath(ctx);
 		CGContextFillPath(ctx);
-		
-		
 	}
-	
+    
+    [self drawPartitions];
 }
+
+-(void)drawPartitions {
+    
+    
+    if (self->m_amRatio == 0 || self->m_amRatio == 1) {
+        return;
+    }
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [UIColor whiteColor].CGColor);
+    
+    // Draw them with a 1.0 stroke width so they are a bit more visible.
+    CGContextSetLineWidth(context, 1);
+    
+    float diameter = self.frame.size.width;
+    float radius = self.frame.size.width/2;
+    float x = radius;
+    float y = radius;
+    
+    if (m_amRatio == 0.5) {
+        
+        CGContextMoveToPoint(context, 0, radius); //start at this point
+        CGContextAddLineToPoint(context, diameter, radius); //draw to this point
+    }
+    else if (m_amRatio == 0.25 || m_amRatio == 0.75) {
+    
+        CGContextMoveToPoint(context, 0, radius);
+        CGContextAddLineToPoint(context, diameter, radius);
+        
+        CGContextMoveToPoint(context, radius, 0);
+        CGContextAddLineToPoint(context, radius, diameter);
+    }
+    else {
+        
+        CGContextMoveToPoint(context, x, y);
+        CGContextAddLineToPoint(context, diameter, radius);
+        
+        float dx = x + ( radius * cos(120 * PI / 180.0) );
+        float dy = y + ( radius * sin(120 * PI / 180.0) );
+        
+        CGContextMoveToPoint(context, x, y);
+        CGContextAddLineToPoint(context, dx, dy);
+        
+        dx = x + ( radius * cos(240 * PI / 180.0) );
+        dy = y + ( radius * sin(240 * PI / 180.0) );
+        
+        CGContextMoveToPoint(context, x, y);
+        CGContextAddLineToPoint(context, dx, dy);
+    }
+    
+    // and now draw the Path!
+    CGContextStrokePath(context);
+}
+
 
 -(void)setAMRatio:(float)amRatio setPMRatio:(float)pmRatio {
     

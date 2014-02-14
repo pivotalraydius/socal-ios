383453d14819b92d4508a6b87d8843e28f624b7c
Hnin Htet <hninhtet@rs-v.com>
2014-02-14 16:54:58 +0800

    Environments and targets for SoCal and SoCal-Dev (localhost development)

---
+++ SoCal/SoCal-Dev/Environment/Environment.m
@@ -0,0 +1,78 @@
+//
+//  Environment.m
+//  Luncheon
+//
+//  Created by Kale on 19/4/13.
+//  Copyright (c) 2013 Rayser. All rights reserved.
+//
+
+#import "Environment.h"
+
+@implementation Environment
+
++(int)CurrentEnvironment {
+    
+    return DEVELOPMENT;
+}
+
++(NSString *)BaseHost {
+    
+    return @"http://mfalcon.local:5000/";
+//    return @"http://raydius-tv.local:5000/";
+}
+
++(NSString *)StagingHost {
+    
+    return @"https://raydius-staging.herokuapp.com/";
+}
+
++(NSString *)Rackspace_Images_CDN_BaseURL {
+    
+    return @"http://3065cbd42358ac65f2d4-e265b11a02ec41d6e2e50f4fb75e3eb1.r97.cf1.rackcdn.com/";
+}
+
++(NSString *)Rackspace_Images_Container {
+    
+    return @"raydius_dev_images";
+}
+
++(NSString *)Rackspace_Place_Images_Container {
+    
+    return @"raydius_dev_place_images";
+}
+
++(NSString *)RackSpace_PlaceImages_CDN_BaseURL {
+    
+    return @"http://4276a6e89bd1f7f33c73-756942a364a72d309fa745e70fac1ce4.r0.cf1.rackcdn.com/";
+}
+
++(NSString *)RackSpace_Avatars_Container {
+    
+    return @"raydius_dev_avatars";
+}
+
++(NSString *)RackSpace_Avatars_CDN_BaseURL {
+    
+    return @"https://ca7f4351facb1c8b333d-84b48088cb4e0b523f14d8b78cb0d538.ssl.cf1.rackcdn.com/";
+}
+
++(void)registerAPN {
+}
+
++(NSString *)Pusher_App_ID {
+    return @"65761";
+}
+
++(NSString *)Pusher_Key {
+    return @"8197ab8e102c7e2ca2eb";
+}
+
++(NSString *)Pusher_Secret {
+    return @"e58e48596fbe758eb605";
+}
+
++(NSString *)LuncheonAppURLBaseHost {
+    return @"luncheonDevelopment://";
+}
+
+@end
--- SoCal/SoCal/Environment/Environment.m
+++ SoCal/SoCal/Environment/Environment.m
@@ -17,8 +17,6 @@
 
 +(NSString *)BaseHost {
     
-//    return @"http://mfalcon.local:5000/";
-//    return @"http://raydius-tv.local:5000/";
     return @"https://socal-staging.herokuapp.com/";
 }
 
@@ -29,48 +27,36 @@
 
 +(NSString *)Rackspace_Images_CDN_BaseURL {
     
-    return @"http://3065cbd42358ac65f2d4-e265b11a02ec41d6e2e50f4fb75e3eb1.r97.cf1.rackcdn.com/";
+    return @"http://cbec2f0c4e77ea508a75-051ad4274f767d2430ba3eee78a801e1.r93.cf1.rackcdn.com/";
 }
 
 +(NSString *)Rackspace_Images_Container {
     
-    return @"raydius_dev_images";
+    return @"raydius_staging_images";
 }
 
 +(NSString *)Rackspace_Place_Images_Container {
     
-    return @"raydius_dev_place_images";
+    return @"raydius_staging_place_images";
 }
 
 +(NSString *)RackSpace_PlaceImages_CDN_BaseURL {
     
-    return @"http://4276a6e89bd1f7f33c73-756942a364a72d309fa745e70fac1ce4.r0.cf1.rackcdn.com/";
+    return @"http://9c5992b7705b19ce1658-2d8133df31036da19af4680fb7319419.r17.cf1.rackcdn.com/";
 }
 
 +(NSString *)RackSpace_Avatars_Container {
     
-    return @"raydius_dev_avatars";
+    return @"raydius_staging_avatars";
 }
 
 +(NSString *)RackSpace_Avatars_CDN_BaseURL {
     
-    return @"https://ca7f4351facb1c8b333d-84b48088cb4e0b523f14d8b78cb0d538.ssl.cf1.rackcdn.com/";
+    return @"http://1452b64aeb44c6f8cfca-944c27f9a2e7b99ec9615bcde91d7c43.r95.cf1.rackcdn.com/";
 }
 
 +(void)registerAPN {
 }
-
-//+(NSString *)Pusher_App_ID {
-//    return @"65761";
-//}
-//
-//+(NSString *)Pusher_Key {
-//    return @"8197ab8e102c7e2ca2eb";
-//}
-//
-//+(NSString *)Pusher_Secret {
-//    return @"e58e48596fbe758eb605";
-//}
 
 +(NSString *)Pusher_App_ID {
     return @"65760";
